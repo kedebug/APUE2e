c8f9cb9fe598ff4789f7defe37844850dde90dda
kedebug <sunweiqq@gmail.com>
2013-10-12 15:33:29 +0800

    avoid being zombie process

---
+++ ch08/zombie.cc
@@ -0,0 +1,23 @@
+#include <apue.h>
+#include <sys/wait.h>
+
+int main() 
+{
+    pid_t pid;
+    
+    if ((pid = fork()) < 0) {
+        err_sys("fork error");
+    } else if (pid == 0) {
+        if ((pid = fork()) < 0) 
+            err_sys("fork error");
+        else if (pid > 0)
+            exit(0);
+
+        sleep(2);
+        printf("second child, parent pid = %d\n", getppid());
+        exit(0);
+    }
+    if (waitpid(pid, NULL, 0) != pid)
+        err_sys("waitpid error");
+    exit(0);
+}
