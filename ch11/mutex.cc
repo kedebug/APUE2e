d3c6bf068df4d58c5f19dc20b5c0e18e1c774183
kedebug <sunweiqq@gmail.com>
2013-10-14 21:12:06 +0800

    using mutex to protect data

---
+++ ch11/mutex.cc
@@ -0,0 +1,41 @@
+#include <apue.h>
+#include <pthread.h>
+
+typedef struct _foo {
+    int f_count;
+    pthread_mutex_t f_lock;
+} foo;
+
+foo *foo_alloc(void)
+{
+    foo *fp;
+
+    if ((fp == malloc(sizeof(foo))) != NULL) {
+        fp->f_count = 1;
+        if (pthread_mutex_init(&fp->f_lock, NULL) != 0) {
+            free(fp);
+            return NULL;
+        }
+        // todo
+    }
+    return fp;
+}
+
+void foo_hold(foo *fp) 
+{
+    pthread_mutex_lock(&fp->f_lock);
+    fp->f_count += 1;
+    pthread_mutex_unlock(&fp->f_lock);
+}
+
+void foo_release(foo *fp) 
+{
+    pthread_mutex_lock(&fp->f_lock);
+    if (--fp->f_count == 0) {
+        pthread_mutex_unlock(&fp->f_lock);
+        pthread_mutex_destroy(&fp->f_lock);
+        free(fp);
+    } else {
+        pthread_mutex_unlock(&fp->f_lock);
+    }
+}
